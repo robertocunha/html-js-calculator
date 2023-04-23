7fcf8b69330c2185e466467659e88b9466ca7759
Roberto Cunha <robertocunha@gmail.com>
2023-04-23 19:14:13 -0300

    Change html elements ids

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let equal_pressed = 0;
 //Refer all buttons excluding AC and DEL
 let button_input = document.querySelectorAll(".input-button");
 //Refer input,equal,clear and erase
-let input = document.getElementById("input");
-let equal = document.getElementById("equal");
-let clear = document.getElementById("clear");
-let erase = document.getElementById("erase");
+let input = document.getElementById("input-id");
+let equal = document.getElementById("equal-id");
+let clear = document.getElementById("clearid");
+let erase = document.getElementById("erase-id");
 
 window.onload = () => {
   input.value = "";
